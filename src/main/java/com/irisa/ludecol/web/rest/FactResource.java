af6373bf8595c7fb434c9a6ecddf9ab4ebbd5818
Dorian <dorian.leroy@irisa.fr>
2015-10-28 17:02:39 +0100

    Changed the way the tips file is parsed

--- src/main/java/com/irisa/ludecol/web/rest/FactResource.java
+++ src/main/java/com/irisa/ludecol/web/rest/FactResource.java
@@ -1,6 +1,7 @@
 package com.irisa.ludecol.web.rest;
 
 import com.irisa.ludecol.security.AuthoritiesConstants;
+import com.irisa.ludecol.web.rest.util.UnicodeReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
@@ -9,9 +10,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.security.RolesAllowed;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
+import java.nio.charset.Charset;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -39,12 +39,23 @@ public class FactResource {
     public Map getFact() {
         log.debug("REST request to get a random fact");
         String result = "<fun fact here>";
+        FileInputStream fis = null;
+        UnicodeReader ur = null;
+        BufferedReader in = null;
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("tipsFR"));
-            List<String> lines = reader.lines().collect(Collectors.toList());
+            fis = new FileInputStream(new File("tipsFR"));
+            ur = new UnicodeReader(fis, "UTF-8");
+            in = new BufferedReader(ur);
+            List<String> lines = in.lines().collect(Collectors.toList());
             result = lines.get(rand.nextInt(lines.size()));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(fis != null) {try {fis.close();} catch (IOException e) {}}
+            if(ur != null) {try {ur.close();} catch (IOException e) {}}
+            if(in != null) {try {in.close();} catch (IOException e) {}}
         }
         return Collections.singletonMap("response", result);
     }
